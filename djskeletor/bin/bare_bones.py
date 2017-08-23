41062e26c3ec441681fec86e6998096e6a3b8dbd
plungerman <plungerman@gmail.com>
2017-08-23 11:18:55 -0500

    updated django version; added try/except example

--- djskeletor/bin/bare_bones.py
+++ djskeletor/bin/bare_bones.py
@@ -5,21 +5,27 @@ import os, sys
 sys.path.append('/usr/lib/python2.7/dist-packages/')
 sys.path.append('/usr/lib/python2.7/')
 sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_1.9/')
+sys.path.append('/data2/django_1.11/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djskeletor.settings")
-
-#import django
-#django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
 
 from django.conf import settings
+
 
 # set up command-line options
 
 def main():
     """
     """
+
+    try:
+        # do something
+    except Exception, e:
+        print "does not exist"
+        print "Exception: {}".format(str(e))
+        sys.exit(1)
+
 
 ######################
 # shell command line
