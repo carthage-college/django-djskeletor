c0ca9d958edf0b24d8bbc4e8061b7dbbf921ba1a
plungerman <plungerman@gmail.com>
2020-08-02 09:54:57 -0500

    converted URL paths/patterns to conform to the new infrastructure in django2.x

--- djskeletor/myapp/urls.py
+++ djskeletor/myapp/urls.py
@@ -1,27 +1,27 @@
-from django.conf.urls import url
+# -*- coding: utf-8 -*-
+
+"""URLs for all views."""
+
+from django.urls import path
 from django.views.generic import TemplateView
 
 from djskeletor.myapp import views
 
+
 urlpatterns = [
-    url(
-        r'^myview/$',
-        views.myview, name='myapp_display_no_pid'
-    ),
-    url(
-        #r'^myview/(?P<username>[-\w]+)/display/$',
-        r'^myview/(?P<pid>\d+)/display/$',
+    path(
+        '<str:pid>/display/',
         views.myview, name='myapp_display'
     ),
-    url(
-        r'^search/$',
-        views.search, name='myapp_search'
+    path(
+        'myview/',
+        views.myview, name='myapp_display_no_pid'
     ),
-    url(
-        r'^myview/success/$',
+    path(
+        'success/',
         TemplateView.as_view(
             template_name='myapp/success.html'
         ),
-        name='myapp_success'
+        name='myapp_success',
     ),
 ]
