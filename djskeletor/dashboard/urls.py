3e7a11962d8ea9d0cff8eadadf07daa5591b384a
plungerman <plungerman@gmail.com>
2020-08-02 09:33:53 -0500

    added a URL path for search

--- djskeletor/dashboard/urls.py
+++ djskeletor/dashboard/urls.py
@@ -2,11 +2,15 @@
 
 """URLs for all views."""
 
-from django.conf.urls import path
+from django.urls import path
 
 from djskeletor.dashboard import views
 
 
 urlpatterns = [
+    path(
+        'search/',
+        views.search, name='dashboard_search'
+    ),
     path('', views.home, name='home'),
 ]
