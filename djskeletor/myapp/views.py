c1866727b71fd9cf61c709f523d45b31037bb26a
plungerman <plungerman@gmail.com>
2020-08-02 09:53:42 -0500

    code cleanup

--- djskeletor/myapp/views.py
+++ djskeletor/myapp/views.py
@@ -1,42 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""URLs for all views."""
+
 from django.conf import settings
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render, get_object_or_404
-
-from djskeletor.myapp.forms import MyForm
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from djtools.utils.mail import send_mail
 
+from djthia.gearup.forms import GraduateForm
 
-def myview(request, pid=None):
+
+def home(request, pid=None):
+    """Home view."""
     if settings.DEBUG:
-        TO_LIST = [settings.SERVER_EMAIL,]
+        to_list = [settings.SERVER_EMAIL]
     else:
-        TO_LIST = [settings.MY_APP_EMAIL,]
-    BCC = settings.MANAGERS
+        to_list = [settings.MY_APP_EMAIL]
 
-    if request.method=='POST':
-        form = MyForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = GraduateForm(request.POST, request.FILES)
         if form.is_valid():
-            data = form.save()
+            grad = form.save()
             email = settings.DEFAULT_FROM_EMAIL
-            if data.email:
-                email = data.email
-            subject = "[Submit] {} {}".format(data.first_name,data.last_name)
+            if grad.email:
+                email = grad.email
+            subject = "[Submit] {0} {1}".format(grad.first_name, grad.last_name)
             send_mail(
-                request,TO_LIST, subject, email,'myapp/email.html', data, BCC
+                request, to_list, subject, email, 'myapp/email.html', grad,
             )
-            return HttpResponseRedirect(
-                reverse_lazy('myapp_success')
-            )
+            return HttpResponseRedirect(reverse_lazy('myapp_success'))
     else:
-        form = MyForm()
-    return render(
-        request, 'myapp/form.html',
-        {'form': form,}
-    )
+        form = GraduateForm()
+    return render(request, 'myapp/form.html', {'form': form})
+
 
 def search(request):
-    return render(
-        request, 'myapp/search.html', {}
-    )
-
+    """Generic search."""
+    return render(request, 'myapp/search.html', {})
